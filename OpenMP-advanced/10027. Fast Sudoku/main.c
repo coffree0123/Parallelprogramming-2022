2eebb1dcb785da1ce84235769df77ae1dd1a843d
coffree0123 <02320013apis@gmail.com>
2022-04-29 14:31:31 +0800

    update fast sudoku

--- OpenMP-advanced/10027. Fast Sudoku/main.c
+++ OpenMP-advanced/10027. Fast Sudoku/main.c
@@ -1,106 +1,108 @@
 #include <omp.h>
 #include <stdio.h>
 
-int zeroNum = 0;
-int zeros[81];
+int zeros[100];
+int total_zero = 0;
+/* rowColConflict */
+int rowColConflict(int try, int row, int col, int sudoku[9][9]) {
+    int conflict = 0;
+    for (int i = 0; i < 9 && !conflict; i++)
+        if (((col != i) && (sudoku[row][i] == try)) ||
+            ((row != i) && (sudoku[i][col] == try)))
+            conflict = 1;
+    return conflict;
+}
+/* blockConflict */
+int blockConflict(int try, int row, int col, int sudoku[9][9]) {
+    int blockRow = row / 3;
+    int blockCol = col / 3;
 
-int conflict(int try, int row, int col, int rowNum[9], int colNum[9],
-             int blockNum[3][3]) {
-    return (rowNum[row] & (1 << (try - 1)) || colNum[col] & (1 << (try - 1)) ||
-            blockNum[row / 3][col / 3] & (1 << (try - 1)));
+    int conflict = 0;
+    for (int i = 0; i < 3 && !conflict; i++)
+        for (int j = 0; j < 3 && !conflict; j++)
+            if (sudoku[3 * blockRow + i][3 * blockCol + j] == try) conflict = 1;
+    return conflict;
 }
 
-void on(int rowNum[9], int colNum[9], int blockNum[3][3], int i, int j,
-        int num) {
-    rowNum[i] |= (1 << (num - 1));
-    colNum[j] |= (1 << (num - 1));
-    blockNum[i / 3][j / 3] |= (1 << (num - 1));
-}
+/* conflict */
+int conflict(int try, int row, int col, int sudoku[9][9]) {
+    // check row conflict
+    for (int i = 0; i < 9 && !conflict; i++)
+        if (((col != i) && (sudoku[row][i] == try)) ||
+            ((row != i) && (sudoku[i][col] == try)))
+            return 1;
 
-void off(int rowNum[9], int colNum[9], int blockNum[3][3], int i, int j,
-         int num) {
-    rowNum[i] &= ~(1 << (num - 1));
-    colNum[j] &= ~(1 << (num - 1));
-    blockNum[i / 3][j / 3] &= ~(1 << (num - 1));
-}
+    // check col conflict
+    int blockRow = row / 3;
+    int blockCol = col / 3;
 
+    for (int i = 0; i < 3 && !conflict; i++)
+        for (int j = 0; j < 3 && !conflict; j++)
+            if (sudoku[3 * blockRow + i][3 * blockCol + j] == try) return 1;
+
+    return 0;
+}
 /* placeNumber */
-int placeNumber(int n, int rowNum[9], int colNum[9], int blockNum[3][3]) {
-    if (n == zeroNum) return 1;
-    int row = zeros[n] / 9;
-    int col = zeros[n] % 9;
+int placeNumber(int n, int sudoku[9][9], int index) {
+    if (index == total_zero) return 1;
+    n = zeros[index];
+    int row = n / 9;
+    int col = n % 9;
+    /* numSolution */
     int numSolution = 0;
-
     for (int try = 1; try <= 9; try++) {
-        if (!conflict(try, row, col, rowNum, colNum, blockNum)) {
-            // sudoku[row][col] = try;
-            on(rowNum, colNum, blockNum, row, col, try);
-            numSolution += placeNumber(n + 1, rowNum, colNum, blockNum);
-            off(rowNum, colNum, blockNum, row, col, try);
+        if (!conflict(try, row, col, sudoku)) {
+            sudoku[row][col] = try;
+            numSolution += placeNumber(n + 1, sudoku, index + 1);
         }
-    }
+    } /* for */
+    sudoku[row][col] = 0;
     return numSolution;
 }
 
 /* main */
 int main(void) {
-    // int sudoku[9][9];
-    // rowNum[1] =
-    int rowNum[9] = {0};
-    int colNum[9] = {0};
-    int blockNum[3][3] = {0};
-    for (int i = 0; i < 9; i++) {
+    int sudoku[9][9];
+    int firstZero = -1;
+    for (int i = 0; i < 9; i++)
         for (int j = 0; j < 9; j++) {
-            int num;
-            scanf("%d", &num);
-            if (num != 0) {
-                on(rowNum, colNum, blockNum, i, j, num);
-            } else {
-                zeros[zeroNum] = i * 9 + j;
-                zeroNum++;
+            scanf("%d", &(sudoku[i][j]));
+            if (sudoku[i][j] == 0 && firstZero == -1) firstZero = i * 9 + j;
+            if (sudoku[i][j] == 0) {
+                zeros[total_zero] = i * 9 + j;
+                total_zero += 1;
             }
         }
-    }
 
     int numSolution = 0;
-#pragma omp parallel for reduction(+ : numSolution) firstprivate(rowNum, colNum, blockNum) collapse(4) schedule(dynamic, 1)
+#pragma omp parallel for collapse(5) reduction(+ : numSolution) firstprivate(sudoku) schedule(dynamic, 1)
     for (int i = 1; i <= 9; i++) {
         for (int j = 1; j <= 9; j++) {
             for (int k = 1; k <= 9; k++) {
-                for (int l = 1; l <= 9; l++) {
-                    if (!conflict(i, zeros[0] / 9, zeros[0] % 9, rowNum, colNum,
-                                  blockNum) &&
-                        !conflict(j, zeros[1] / 9, zeros[1] % 9, rowNum, colNum,
-                                  blockNum) &&
-                        !conflict(k, zeros[2] / 9, zeros[2] % 9, rowNum, colNum,
-                                  blockNum) &&
-                        !conflict(l, zeros[3] / 9, zeros[3] % 9, rowNum, colNum,
-                                  blockNum)) {
-                        on(rowNum, colNum, blockNum, zeros[0] / 9, zeros[0] % 9,
-                           i);
-                        on(rowNum, colNum, blockNum, zeros[1] / 9, zeros[1] % 9,
-                           j);
-                        on(rowNum, colNum, blockNum, zeros[2] / 9, zeros[2] % 9,
-                           k);
-                        on(rowNum, colNum, blockNum, zeros[3] / 9, zeros[3] % 9,
-                           l);
-
-                        numSolution += placeNumber(4, rowNum, colNum, blockNum);
-
-                        off(rowNum, colNum, blockNum, zeros[3] / 9,
-                            zeros[3] % 9, l);
-                        off(rowNum, colNum, blockNum, zeros[2] / 9,
-                            zeros[2] % 9, k);
-                        off(rowNum, colNum, blockNum, zeros[1] / 9,
-                            zeros[1] % 9, j);
-                        off(rowNum, colNum, blockNum, zeros[0] / 9,
-                            zeros[0] % 9, i);
+                for (int t = 1; t <= 9; t++) {
+                    for (int u = 1; u <= 9; u++) {
+                        if (!conflict(i, zeros[0] / 9, zeros[0] % 9, sudoku) &&
+                            !conflict(j, zeros[1] / 9, zeros[1] % 9, sudoku) &&
+                            !conflict(k, zeros[2] / 9, zeros[2] % 9, sudoku) &&
+                            !conflict(t, zeros[3] / 9, zeros[3] % 9, sudoku) &&
+                            !conflict(u, zeros[4] / 9, zeros[4] % 9, sudoku)) {
+                            sudoku[zeros[0] / 9][zeros[0] % 9] = i;
+                            sudoku[zeros[1] / 9][zeros[1] % 9] = j;
+                            sudoku[zeros[2] / 9][zeros[2] % 9] = k;
+                            sudoku[zeros[3] / 9][zeros[3] % 9] = t;
+                            sudoku[zeros[4] / 9][zeros[4] % 9] = u;
+                            numSolution += placeNumber(firstZero, sudoku, 5);
+                        }
+                        sudoku[zeros[0] / 9][zeros[0] % 9] = 0;
+                        sudoku[zeros[1] / 9][zeros[1] % 9] = 0;
+                        sudoku[zeros[2] / 9][zeros[2] % 9] = 0;
+                        sudoku[zeros[3] / 9][zeros[3] % 9] = 0;
+                        sudoku[zeros[4] / 9][zeros[4] % 9] = 0;
                     }
                 }
             }
         }
     }
-
     printf("%d\n", numSolution);
     return 0;
 }
